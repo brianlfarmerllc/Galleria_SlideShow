00389c766808b5bc4fab45e94f1825e53a7d18cc
brianlfarmerllc <brianlfarmerllc@gmail.com>
2021-08-07 02:18:47 -0500

    disable exit animations - not working after build

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,9 @@ import "../styles/globals.scss";
 function MyApp({ Component, pageProps, router }) {
   return (
     <AnimatePresence onExitComplete={() => window.scrollTo(0, 0)} exitBeforeEnter>
-      <motion.div key={router.asPath}>
-        <Component {...pageProps} />
-      </motion.div>
+      {/* <motion.div key={router.asPath}> */}
+      <Component {...pageProps} />
+      {/* </motion.div> */}
     </AnimatePresence>
   );
 }
